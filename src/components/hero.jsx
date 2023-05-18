d160f8a33d961d2019889ac6039c895b5ac94ff7
Summer <swatson9008@yahoo.com>
2023-05-18 13:03:24 -0400

    slight css adjustments

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,8 +2,13 @@ import photogrid from '../assets/photo-grid.png';
 
 export default function Hero(){
     return(
-        <div className='HeroGrid'>
+        <div className='Hero'>
             <img src={photogrid} alt="photogrids" className='HeroGridIMG'/>
+            <div className='Experiences'>
+                <h1>Online Experiences</h1>
+                <span>Join unique interactive activities led by 
+                one-of-a-kind hosts—all without leaving home.</span>
+            </div>
         </div>
     )
 }
